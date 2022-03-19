ebc21a28e3feb05e31b9e1b99fce9d55e269ec3f
Jim Gagliardi <jvgagliardi98@outlook.com>
2022-03-18 21:00:48 -0400

    Working on Zeppelin

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Certification_Option_C/Scripts/Enemies/Zeppelin/ZeppelinAI.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Certification_Option_C/Scripts/Enemies/Zeppelin/ZeppelinAI.cs
@@ -4,5 +4,83 @@ using UnityEngine;
 
 public class ZeppelinAI : Enemy
 {
+    [Header("VFX Prefabs")]
+    [SerializeField] private GameObject _stageTwoDamaagePrefab;
+    [SerializeField] private GameObject _stageThreeDamaagePrefab;
 
+    [Header("Boss Debug")]
+    [SerializeField] private Stage _stage;
+
+    private bool _secondStage;
+    private bool _thirdStage;
+
+    private int _maxHealth;
+
+    private ZeppelinMovement _zeppelinMovement;
+
+    private enum Stage
+    {
+        Basic,
+        Modified,
+        Final
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        _zeppelinMovement = GetComponent<ZeppelinMovement>();
+        _maxHealth = Health;
+        _stage = Stage.Basic;
+    }
+
+    private void Update()
+    {
+        SetStage();
+
+        Debug.Log("Stage: " + _stage);
+    }
+
+    private void SetStage()
+    {
+        if (_zeppelinMovement != null)
+        {
+            switch (_stage)
+            {
+                case Stage.Basic:
+                    _zeppelinMovement.BasicMovement();
+                    //Shoot phase 1
+                    break;
+                case Stage.Modified:
+                    _zeppelinMovement.ModifiedMovement();
+                    //Instantiate damage 1
+                    //shoot phase 2
+                    break;
+                case Stage.Final:
+                    _zeppelinMovement.FinalMovement();
+                    //Instantiate damage 2
+                    //shoot phase 3
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public override void Damage(int damageAmount)
+    {
+        base.Damage(damageAmount);
+
+        if (Health >= (_maxHealth * 0.66f))
+        {
+            return;
+        }
+        else if (Health < (_maxHealth * 0.66f) && Health >= (_maxHealth * 0.33f) /*&& _secondStage*/)
+        {
+            _stage = Stage.Modified;
+        }
+        else if (Health < (_maxHealth * 0.33f) /*&& _thirdStage*/)
+        {
+            _stage = Stage.Final;
+        }
+    }
 }
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Certification_Option_C/Scripts/Enemies/Zeppelin/ZeppelinMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeppelinMovement : MonoBehaviour
+{
+    [Header("Adjustable Values")]
+    [SerializeField] private float _movementSpeed = 10.0f;
+
+    [Header("Prefabs")]
+    [SerializeField] private Transform _start;
+
+    private bool _canMove = true;
+
+    private Vector3 _target;
+
+    private Player _player;
+
+    private void Start()
+    {
+        Init();
+        SetTargetPosition();
+    }
+
+    private void Init()
+    {
+        _player = GameObject.Find("/Player Manager/Player").GetComponent<Player>();
+    }
+
+    private void SetTargetPosition()
+    {
+        if (_player != null)
+        {
+            _target = _player.transform.position;
+        }
+        else
+        {
+            _target = Vector3.zero;
+        }
+    }
+
+    public void BasicMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _start.position, Time.deltaTime * _movementSpeed);
+    }
+
+    public void ModifiedMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _movementSpeed * Time.deltaTime);
+
+        float distance = Vector3.Distance(transform.position, _target);
+
+        if (distance < 1.0f)
+        {
+            SetTargetPosition();
+        }
+    }
+
+    public void FinalMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _movementSpeed * 2 * Time.deltaTime);
+
+        float distance = Vector3.Distance(transform.position, _target);
+
+        if (distance < 1.0f)
+        {
+            SetTargetPosition();
+        }
+    }
+}
